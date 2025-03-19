beeac1631dfb0609fd3abea99b60f557ccef788b
Muhammad Zubair <mzubairabc947@gmail.com>
2025-03-19 13:57:23 +0500

    seven commit

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,8 +1,30 @@
+// import React from "react";
+// import LoginComponent from "../../../components/LoginComponent";
+
+// const LoginPage = () => {
+//   return (
+//     <div>
+//       <LoginComponent />
+//     </div>
+//   );
+// };
+
+// export default LoginPage;
+
+// export function generateMetadata() {
+//   return {
+//     title: "Login Page for User",
+//     description: "Description for login page",
+//   };
+// }
 import React from "react";
 import LoginComponent from "../../../components/LoginComponent";
-const LoginPage = () => {
+
+const LoginPage = ({ metadata }) => {
   return (
     <div>
+      <h1>{metadata.title}</h1>
+      <p>{metadata.description}</p>
       <LoginComponent />
     </div>
   );
@@ -10,9 +32,13 @@ const LoginPage = () => {
 
 export default LoginPage;
 
-export function generateMetadata() {
+export async function getStaticProps() {
   return {
-    title: "Login Page for User",
-    description: "Description for login page",
+    props: {
+      metadata: {
+        title: "Login Page for User",
+        description: "Description for login page",
+      },
+    },
   };
 }
